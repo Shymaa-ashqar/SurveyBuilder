9b2f231a00843b06bafed87652d9df64f5830679
Shaimaa Alashqar <Shaimaa.Alashqar@Realsoft-me.com>
2022-03-15 17:40:11 +0300

    list view edited

--- src/app/cards-list/cards-list.component.ts
+++ src/app/cards-list/cards-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, ElementRef, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DialogComponent } from '../dialog/dialog.component';
 import { DataService } from '../services/data.service';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort, Sort} from '@angular/material/sort';
 import {LiveAnnouncer} from '@angular/cdk/a11y';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 @Component({
@@ -12,82 +13,66 @@ import {LiveAnnouncer} from '@angular/cdk/a11y';
   templateUrl: './cards-list.component.html',
   styleUrls: ['./cards-list.component.sass']
 })
-export class CardsListComponent implements OnInit {
+export class CardsListComponent implements OnInit,AfterViewInit {
 
   fetchedData!: any[];
   dialogReceivedObject!:any;
   ViewValue:string="grid";
+  dataSource:any;
   @ViewChild('radioBtn')btn!: any;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private data: DataService,private dialog: MatDialog,private _liveAnnouncer: LiveAnnouncer) { }
 
   ngOnInit(): void {
     this.data.sendGetRequest().subscribe((res:any)=>{
       this.fetchedData = res[0];
+      this.dataSource = new MatTableDataSource(this.fetchedData);
     }) 
     console.log(this.fetchedData)
+    this.dataSource.paginator = this.paginator;
+    // console.log(this.dataSource);
   }  
-  // posts.component.ts
-
-
-//   ngDoCheck(){
-//     // only run when property "data" changed
-    
-//     this.data.sendGetRequest().subscribe((res:any)=>{
-//       this.fetchedData = res[0];
-//     }) 
-    
-// }
-
+  ngAfterViewInit() {
+    // this.dataSource.paginator = this.paginator;
+  }
 
   displayedColumns: string[] = ['radio', 'icon','TemplateName', 'From', 'To', 'Period'];
 
-
+// console data
   test(): void {
-  console.log(this.fetchedData)
-      
-
+  console.log(this.dataSource)
   }
+// receive selected card and store it in dialogReceivedObject
   print(receivedData:any){
     this.dialogReceivedObject=receivedData
      }
+
   openDialog() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-
     dialogConfig.data =this.dialogReceivedObject.TemplateName
-   
-
     this.dialog.open(DialogComponent, dialogConfig);
 }
-
+// toggle between list and grid view
 changeView(item:string){
 this.ViewValue=item;
 console.log(this.ViewValue)
 }
+// select card from list view
 SelectedSurvey(data:any,item:any){  
   this.btn=item.checked;
 }
 
-@ViewChild(MatSort) sort!: MatSort;
-
-ngAfterViewInit() {
-  // this.fetchedData.sort = this.sort;
-}
-
-/** Announce the change in sort state for assistive technology. */
-announceSortChange(sortState: Sort) {
-  // This example uses English messages. If your application supports
-  // multiple language, you would internationalize these strings.
-  // Furthermore, you can customize the message to add additional
-  // details about the values being sorted.
-  if (sortState.direction) {
-    this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-  } else {
-    this._liveAnnouncer.announce('Sorting cleared');
-  }
-}
+// announceSortChange(sortState: Sort) {
+//   if (sortState.direction) {
+//     this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+//   } else {
+//     this._liveAnnouncer.announce('Sorting cleared');
+//   }
+// }
 
 
 }
